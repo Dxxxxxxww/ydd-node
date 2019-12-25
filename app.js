e65674ff49ac615787e4d6667d6701398cd80e1a
Dxxxxxxww <gotu2018@163.com>
2019-12-25 08:50:45 +0800

    初识 koa-router

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const Koa = require('koa')
+const Router = require('koa-router')
 
 const app = new Koa()
+const router = new Router()
 
-app.use(async (ctx, next) => {
-  const a = await next()
+router.get('/hello', (ctx, next) => {
+  ctx.body = { key: 'world' }
 })
 
-app.use(async (ctx, next) => {
+app.use(router.routes())
+
+/* 
+  app.use(async (ctx, next) => {
+    const a = await next()
+  })
+
+  app.use(async (ctx, next) => {
   const axios = require('axios')
   const start = Date.now()
   //await 求值关键字， 1.对 promise 对象求值 普通表达式直接返回。 2.阻塞当前线程
@@ -15,5 +24,5 @@ app.use(async (ctx, next) => {
   console.log(end - start)
   return 'yd'
 })
-
+ */
 app.listen(3000)
