a213e14510fcc4a000d162250d79ef0e4f62ba33
Dxxxxxxww <gotu2018@163.com>
2020-01-08 23:22:18 +0800

    使用 require-directory 批量导入路由，并自动监听 by dxw

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const Koa = require('koa')
-const book = require('./api/v1/book')
-const classic = require('./api/v1/classic')
+const requireDirectory = require('require-directory')
+const Router = require('koa-router')
 
 const app = new Koa()
 
-app.use(book.routes())
-app.use(classic.routes())
+requireDirectory(module,'/api', {
+  visit: whenModulesLoad
+})
+
+const whenModulesLoad = function (obj) {
+  if (obj instanceof Router) {
+    app.use(obj.routes())
+  }
+}
 
 // router.get('/hello', (ctx, next) => {
 //   ctx.body = { key: 'world' }
