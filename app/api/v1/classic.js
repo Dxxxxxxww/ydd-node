683de682c8685507d293ee7b10261f2a79d9310f
Dxxxxxxww <gotu2018@163.com>
2020-03-16 23:18:13 +0800

    sequelize 序列化 by dxw

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -3,7 +3,7 @@ const Router = require('koa-router')
 const { Auth } = require('../../../middlewares/auth')
 const { AuthLevel } = require('../../lib/enum')
 const { Flow } = require('../../models/flow')
-
+const { Art } = require('../../models/art')
 
 const router = new Router({
 	prefix: '/v1/classic'
@@ -30,7 +30,17 @@ router.get('/latest', new Auth(AuthLevel.USER).m, async (ctx, next) => {
 	const flow = await Flow.findOne({
 		order: [['index', 'DESC']]
 	})
-	ctx.body = flow
+	const art = await Art.getData(flow.art_id, flow.type)
+	// 直接这样定义一个 index 属性是没有用的
+	// 这里涉及到序列化的问题
+	// 序列化是指把某一种语言的相关对象转换成 json
+	// 这里的 art 不是普通的字面量对象，它是一个数据库查询结果的返回对象，可以打印查看 art 的解构
+	// 这里只有 dataValues 里的字段会作为 json 返回回去
+	// 这样写其实不太好，因为如果不看 art 数据结构源码是不知道要这样写的，这样写可以成功是因为 js 是一门动态语音
+	// 可以使用 sequelize 的方法 setDataValue
+	// art.dataValues.index = flow.index
+	art.setDataValue('index', flow.index)
+	ctx.body = art
 })
 
 module.exports = { router }
